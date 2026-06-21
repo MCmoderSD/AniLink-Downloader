a252bfc16ae581605101992cd5a5a78d2441fa3f
MCmoderSD <mail@mcmodersd.de>
2026-06-21 10:33:11 +0200

    Release 2.0.1 - Version and Dependency Update
    - Updated application version to 2.0.1
    - Updated Debrid-Link-API dependency version to 1.1.2
    - Updated JsonUtility dependency version to 1.5.6

--- src/main/java/de/MCmoderSD/main/Main.java
+++ src/main/java/de/MCmoderSD/main/Main.java
@@ -11,7 +11,7 @@ import java.util.*;
 public class Main {
 
     // Version
-    public static final String VERSION = "2.0.0";
+    public static final String VERSION = "2.0.1";
 
     // Config
     public static long DELAY;
